233349bf2e71690e49ced1a80f9ba5177e23d88f
Tanya <tanyshka_zp@ukr.net>
2017-05-18 00:01:52 +0300

    added task 9

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@
  Напишите аналог встроенного метода forEach для работы с массивами(Должна передавать: элемент, индекс, массив)
  */
 
-let arr1 = [1,2,3,4,5];
+let arr1 = [1, 2, 3, 4, 5];
 
-function forEach(array, fn){
-    for(let i=0; i<array.length; i++){
+function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) {
         fn(array[i], i, array);
     }
 }
 
-function propertySearch(item, ind, arr){
+function propertySearch(item, ind, arr) {
     console.log('arr['+ind+']:' + arr[ind] + ';' + ' ' + 'ind:' + ind + ';' + '  ' + 'arr: ' + arr);
 }
 
@@ -24,81 +24,121 @@ forEach(arr1, propertySearch);
  Напишите аналог встроенного метода map для работы с массивами
  */
 
-let basicArr = [7,8,9];
-let res;
+let basicArr = [7, 8, 9];
 
 function map(arr, propertySearch) {
     let newArr = [];
-    for(let i=0; i < arr.length; i++){
-        propertySearch(arr[i], i, arr);
-        newArr.push(res);
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(propertySearch(arr[i], i, arr));
     }
+
     return newArr;
 }
 function propertyFunc(item, ind, array) {
-    res = array[ind] * 2;
 
+    return array[ind] * 2;
 }
 map(basicArr, propertyFunc);
-
-
-
-
-
-
-
-/*let basicArr = [7,8,9];
-let res;
-let newArr = [];
-
-function map(arr, propertySearch) {
-
-
-    for(let i=0; i < arr.length; i++){
-        propertySearch(arr[i], i, arr);
-        newArr[i] = arr[i];
-        newArr.push(res);
-
-    }
-    return newArr;
-}
-
-function propertyFunc(item, ind, array) {
-    res = array[ind] * 2;
-
-}
-
-map(basicArr, propertyFunc);*/
-
-
-
-
-
-
-
 
 /*
  Задача 3:
  Напишите аналог встроенного метода reduce для работы с массивами
  */
 
-let thisArr = [1,2,3,4,5];
+/* let thisArr = [1, 2, 3, 4, 5];
 let init = 2;
 
 function reduce(array, fn, initial) {
+    for(let i = 0; i < array.length; i++){
+
+    }
+
     return fn(array, initial);
 }
 
-function workingFunction(thisArr, init){
-
+function workingFunction(thisArr, init) {
     let result = init;
-    for(let i = 0; i < thisArr.length; i++){
+
+    for (let i = 0; i < thisArr.length; i++) {
         result += thisArr[i];
     }
+
     return result;
 }
 
-reduce(thisArr, workingFunction, init);
+reduce(thisArr, workingFunction, init);*/
+
+// -1) должна вызывать функцию для каждого элемента и передавать предыдущий результат первым аргументом
+// -2) должна учитывать initial;
+// -3) если initial не указан, то при первой итерации в prev передается первый элемент массива;
+// 4) +должна передавать элемент вторым аргументом;
+// 5) +должна передавать индекс элемента третьим аргументом;
+// 6) +должна передавать сам массив четвертым аргументом;
+// 7) +не должна изменять оригинальный массив;
+// -8) общая проверка работоспособности.
+/* let thisArr = [1, 2, 3, 4, 5];
+let init = 2;
+
+function reduce(array, fn, initial) {
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (i = 0 && initial == undefined) {
+            prev = array[i];
+            console.log('prevFromIf', prev);
+        }
+        else if (i = 0 && initial !== undefined) {
+            prev = initial;
+            console.log('prevFromIf', prev);
+        }
+        fn(prev, array[i], i, array);
+    }
+
+    return prev;
+}
+
+function myFunc(previousValue, currentItem, index, arr) {
+
+    previousValue = previousValue + currentItem;
+
+    return previousValue;
+}
+
+reduce(thisArr, myFunc, init);*/
+
+let thisArr = [1, 2, 3, 4, 5];
+//let init = 0;
+
+function reduce(array, fn, initial) {
+    //debugger;
+
+    let prev;
+    let i;
+    let res = 0;
+
+    if (initial == undefined) {
+        prev = array[0];
+        i = 1;
+    }
+    else {
+        prev = initial;
+        i = 0;
+    }
+
+    for (i; i < array.length; i++) {
+
+        res += fn(prev, array[i], i, array);
+
+        console.log('res', res);
+    }
+    return res;
+}
+function myFunc(previousValue, currentItem, index, arr) {
+    let result = previousValue + currentItem;
+    return result;
+}
+reduce(thisArr, myFunc, 7);
 
 /*
  Задача 4:
@@ -124,11 +164,11 @@ deleteProperty(dog, 'color');
  Функция должна проверить существует ли укзаанное свойство в указанном объекте
  */
 function hasProperty(obj, prop) {
-    if(obj.hasOwnProperty(prop)){
+    if (obj.hasOwnProperty(prop)) {
+
         return true;
-    } else{
-        return false;
     }
+        return false;
 }
 
 let car = {
@@ -140,19 +180,18 @@ let car = {
 console.log(hasProperty(car, 'type'));
 console.log(hasProperty(car, 'wheel'));
 
-
 /*
  Задача 6:
  Функция должна получить все перечисляемые свойства объекта и вернуть их в виде массива
  */
 function getEnumProps(obj) {
     let propsInArr = [];
-    for(let prop in obj){
-        propsInArr.push(prop);
-        //console.log(prop);
-    }
-    return propsInArr;
 
+    for (let prop in obj) {
+        propsInArr.push(prop);
+    }
+
+    return propsInArr;
 }
 
 let book = {
@@ -171,76 +210,77 @@ getEnumProps(book);
 
 function upperProps(obj) {
     let arr = [];
+
     for (let key in obj) {
         arr.push(key.toUpperCase());
     }
+
     return arr;
 }
 
 let human = {
-    firstName: "Chris",
-    lastName: "Burn",
+    firstName: 'Chris',
+    lastName: 'Burn',
     age: 27,
-    language: "eng"
+    language: 'eng'
 };
 
 upperProps(human);
-
-
-
-
 
 /*
  Задача 8 *:
  Напишите аналог встроенного метода slice для работы с массивами
  */
 
-let arr = [0,1,2,3,4,5,6,7,8];
-let returnedArr = [];
+/* let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let returnedArr = [];*/
 
-function slice(array, from, to) {
-
-    function negativeFrom(par){
+function slice(array, from, to) {}
+/*
+    function negativeFrom(par) {
         from = (array[array.length - 1] + from);
         console.log('-from: ', from);
+
         return par;
     }
 
-    function negativeTo(par){
+    function negativeTo(par) {
         to = (array[array.length - 1] + to);
         console.log('-to: ', to);
+
         return par;
     }
 
-    if(from < 0 && to < 0){
-
+    if (from < 0 && to < 0) {
         negativeFrom(from);
         negativeTo(to);
 
-        for(let i = from; i < to; i++){
+        for (let i = from; i < to; i++) {
             returnedArr.push(array[i]);
         }
+
         return returnedArr;
     }
 
-    else if(from >= 0 && to < 0){
+    else if (from >= 0 && to < 0) {
         negativeTo(to);
 
-        for(let i = from; i < to; i++){
+        for (let i = from; i < to; i++) {
             returnedArr.push(array[i]);
         }
-        return returnedArr;
-    }
 
-    else{
-        for(let i = from; i < to; i++){
+        return returnedArr;}
+
+    else {
+        for (let i = from; i < to; i++) {
             returnedArr.push(array[i]);
         }
+
         return returnedArr;
     }
 }
 
-slice(arr, 1, 7);
+slice(arr, 1, 7);*/
 
 /*
  Задача 9 *:
@@ -248,8 +288,29 @@ slice(arr, 1, 7);
  Proxy должен перехватывать все попытки записи значений свойств и возводить
  это значение в квадрат
  */
-function createProxy(obj) {
+
+let sportShopPrices = {
+    sneakers: 2,
+    ball: 3,
+    trousers: 4,
+    tShirt: 5
+};
+
+function createProxy(object) {
+
+    let proxy = new Proxy(object, {
+        set(obj, prop, value) {
+
+            obj[prop] = value ** 2;
+
+            return true;
+        }
+    });
+
+    return proxy;
 }
+
+createProxy(sportShopPrices, { a: 7, b: 8 });
 
 export {
     forEach,
